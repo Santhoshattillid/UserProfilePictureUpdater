298134129ed11504789761d32b29a7de06a83367
unknown <alourdaraj@DEV1.dev.local>
2012-11-09 05:54:31 +0000

    initial upload to git

---
+++ UserProfilePictureUpdater/Layouts/UserProfilePictureUpdater/UpdateUserProfileImage.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint.WebControls;
+using UserProfilePictureUpdater.UserProfileService;
+
+namespace UserProfilePictureUpdater.Layouts.UserProfilePictureUpdater
+{
+    public partial class UpdateUserProfileImage : LayoutsPageBase
+    {
+        public string ProfilePictureLibrary = "ProfilePictures";
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+        protected void BtnRegister_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (IsPostBack && SPContext.Current != null)
+                {
+                    if (Fuprofileimage.HasFile)
+                    {
+
+                        var web = SPContext.Current.Site.RootWeb;
+
+                        // uploading image to document library
+                        var list = web.Lists.TryGetList(ProfilePictureLibrary);
+                        if(list == null)
+                        {
+                            web.AllowUnsafeUpdates = true;
+                            var listUID = web.Lists.Add(ProfilePictureLibrary, string.Empty, SPListTemplateType.PictureLibrary);
+                            web.Update();
+                            web.AllowUnsafeUpdates = false;
+                            list = web.Lists[listUID];
+                        }
+                        if (list != null)
+                        {
+                            // now upload the file to document library
+                            var file = list.RootFolder.Files.Add(list.RootFolder.Url + "/" + Fuprofileimage.FileName,Fuprofileimage.FileBytes,true);
+                            file.Update();
+                            var userProfileService = new UserProfileService.UserProfileService
+                                                         {
+                                                             UseDefaultCredentials = true,
+                                                             Url = web.Site.Url + "/_vti_bin/userprofileservice.asmx",
+                                                             AllowAutoRedirect = true
+                                                         };
+                            foreach (var account in PEaccount.Accounts)
+                            {
+                                var newdata = new PropertyData[1];
+                                newdata[0] = new PropertyData { Name = "PictureURL", Values = new ValueData[1] };
+                                newdata[0].Values[0] = new ValueData { Value = web.Url + "/" + file.Url };
+                                newdata[0].IsValueChanged = true;
+                                userProfileService.ModifyUserPropertyByAccountName(account.ToString(), newdata);
+                            }
+                            LblError.Text = "Profile pictures updated successfully.";
+                        }
+                        else
+                            LblError.Text = "Profile pictures list not available";
+                    }
+                    else
+                        LblError.Text = "Please select the profile picture and then update!";
+                }
+            }
+            catch (Exception ex)
+            {
+                LblError.Text = "Error - " + ex.Message;
+                SPDiagnosticsService.Local.WriteTrace(0, new SPDiagnosticsCategory("SkillLabTestScheduling", TraceSeverity.Monitorable, EventSeverity.Error), TraceSeverity.Monitorable, ex.Message, new object[] { ex.StackTrace });
+            }
+        }
+    }
+}
---
+++ UserProfilePictureUpdater/pkgobj/Debug/TokenReplaceFolder/Layouts/UserProfilePictureUpdater/UpdateUserProfileImage.aspx
@@ -0,0 +1,47 @@
+<%@ Assembly Name="UserProfilePictureUpdater, Version=1.0.0.0, Culture=neutral, PublicKeyToken=737404fe71f897ae" %>
+<%@ Import Namespace="Microsoft.SharePoint.ApplicationPages" %>
+<%@ Register Tagprefix="SharePoint" Namespace="Microsoft.SharePoint.WebControls" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="Utilities" Namespace="Microsoft.SharePoint.Utilities" Assembly="Microsoft.SharePoint, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Register Tagprefix="asp" Namespace="System.Web.UI" Assembly="System.Web.Extensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" %>
+<%@ Import Namespace="Microsoft.SharePoint" %>
+<%@ Assembly Name="Microsoft.Web.CommandUI, Version=14.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c" %>
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="UpdateUserProfileImage.aspx.cs" Inherits="UserProfilePictureUpdater.Layouts.UserProfilePictureUpdater.UpdateUserProfileImage" DynamicMasterPageFile="~masterurl/default.master" %>
+<asp:Content ID="PageTitle" ContentPlaceHolderID="PlaceHolderPageTitle" runat="server">
+Update User profile picture
+</asp:Content>
+<asp:Content ID="PageTitleInTitleArea" ContentPlaceHolderID="PlaceHolderPageTitleInTitleArea" runat="server" >
+Update User profile picture
+</asp:Content>
+<asp:Content ID="PageHead" ContentPlaceHolderID="PlaceHolderAdditionalPageHead" runat="server">
+    <link href="Stylesheet.css" rel="stylesheet" type="text/css" />
+</asp:Content>
+<asp:Content ID="Main" ContentPlaceHolderID="PlaceHolderMain" runat="server">
+    <div class="container">
+        <ul>
+            <li>
+                <h3>Update user profile picture</h3>
+            </li>
+            <li>
+                <label>User account</label>
+            </li>
+            <li>
+                <SharePoint:PeopleEditor ID="PEaccount" ControlMode="New" 
+                runat="server" AllowEmpty="False" AutoPostBack="True"   SelectionSet="User" CssClass="peopleeditor" 
+                Width="300px" />
+            </li>
+            <li>
+                <label>Select profile image</label>
+            </li>
+            <li>
+                <asp:FileUpload ID="Fuprofileimage" runat="server"  />
+            </li>
+            <li>
+                <asp:Label runat="server" ID="LblError" CssClass="ErrorText" ></asp:Label>
+            </li>
+            <li>
+                <asp:Button ID="BtnUpdate" runat="server" Text="Update" OnClick="BtnRegister_Click" />
+            </li>
+        </ul>
+    </div>
+</asp:Content>
+
